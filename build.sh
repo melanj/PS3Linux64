a753fc785bce5262643de9052d741542d2f009e7
Melan Jayasinghage <melan@apache.org>
2025-03-20 21:22:01 +0900

    Added kernel build steps

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ readonly TARGET_DIR=./dist
 readonly MIRROR=https://deb.debian.org/debian-ports
 readonly KEYRING=/usr/share/keyrings/debian-ports-archive-keyring.gpg
 readonly TAR_IMAGE=./rootfs.tar.xz
+readonly PATCH_DIR=kernel_patches
+readonly KERNEL_VER=6.6.67
+readonly CONFIG_FILE=config-$KERNEL_VER-PS3
 
 # Ensure target directory exists
 mkdir -p "$TARGET_DIR"
@@ -22,6 +25,11 @@ sudo mount --bind /dev "$TARGET_DIR/dev"
 sudo mount --bind /proc "$TARGET_DIR/proc"
 sudo mount --bind /sys "$TARGET_DIR/sys"
 
+#copying kernel patches
+echo "copying kernel patches and config.."
+cp -r $PATCH_DIR $TARGET_DIR/tmp/
+cp -r $CONFIG_FILE $TARGET_DIR/tmp/
+
 # Chroot into the target directory and configure the system
 sudo chroot "$TARGET_DIR" /bin/bash <<EOF
 
@@ -30,10 +38,32 @@ apt modernize-sources -y
 apt update
 apt install -y debian-ports-archive-keyring
 apt update
-apt install -y wget curl python3 dialog file git kmod openssh-server sudo vim iputils-ping net-tools network-manager
+apt install -y wget curl python3 dialog file git kmod openssh-server sudo vim iputils-ping net-tools network-manager \
+ build-essential flex bison libncurses-dev libssl-dev bc dwarves cpio rsync libelf-dev libdebuginfod-dev
 
 # Enable NetworkManager on boot
 systemctl enable NetworkManager
+
+# fetching and building the kernel
+wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$KERNEL_VER.tar.xz -P /usr/src/
+cd /usr/src
+tar -xf linux-$KERNEL_VER.tar.xz
+rm linux-$KERNEL_VER.tar.xz
+cd linux-$KERNEL_VER
+cp /tmp/$CONFIG_FILE .config
+for patch in "/tmp/$PATCH_DIR"/*.patch ; do [ -f "\$patch" ] && patch -p1 < "\$patch"; done
+
+KBUILD_BUILD_HOST="\$(hostname --fqdn)"
+
+make -j\$(nproc) olddefconfig
+make -j\$(nproc)
+make -j\$(nproc) install
+make -j\$(nproc) modules_install
+make -j\$(nproc) headers_install
+
+cd ~
+# remove the Linux source directory to reduce the image size, although I wish I could keep it.
+rm -rf /usr/src/linux-$KERNEL_VER
 
 # Clean up
 apt clean
